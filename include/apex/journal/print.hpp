4a61b41531b1ba4d9626130ef285b934c68eaeec
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-07-06 11:37:24 -0700

    ⚗ Modify journal::print to use source_location by default

--- include/apex/journal/print.hpp
+++ include/apex/journal/print.hpp
@@ -3,10 +3,18 @@
 
 #include <apex/journal/priority.hpp>
 #include <apex/core/string.hpp>
+#include <apex/core/source.hpp>
 
 namespace apex::journal {
 
-template <class... Args> void print (apex::zstring_view, Args&&...);
+// This is a 'hack' taken from https://cor3ntin.github.io/posts/variadic/
+// because we cannot have source_location as the last parameter given.
+// Sadly, this *can* lead to template bloat. Some work is needed to make sure
+// this won't happen in practice.
+template <class... Args>
+struct print final {
+  print (apex::zstring_view, Args&&..., apex::source_location location = apex::source_location::current());
+};
 
 } /* namespace apex::journal */
 
