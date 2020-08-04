49f136a1642460d6b4929a9e0889d2751869bf00
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-08-04 10:53:34 -0700

    ✨ Add initial apex::iter::write_into
    This allows us to have output_iterators supported via our
    mixin::iterator type

--- include/apex/iter/write-into.hpp
+++ include/apex/iter/write-into.hpp
@@ -6,7 +6,28 @@ namespace apex::detail::iter::write_into {
 template <class T, class U>
 void write_into (T&&, U&&) = delete;
 
-struct function final { };
+struct function final {
+  template <class T, class U>
+  constexpr void operator () (T&& target, U&& value) const
+  noexcept(noexcept(function::invoke(static_cast<T&&>(target), static_cast<U&&>(value), prefer<1>))) {
+    function::invoke(static_cast<T&&>(target), static_cast<U&&>(value), prefer<1>);
+  }
+private:
+  template <class T, class U>
+  requires requires (T&& x, U&& y) { static_cast<T&&>(x).write_into(static_cast<U&&>(y)); }
+  constexpr void invoke (T&& target, U&& value, preference<1>)
+  noexcept(noexcept(static_cast<T&&>(target).write_into(static_cast<U&&>(value)))) {
+    static_cast<T&&>(target).write_into(static_cast<U&&>(value));
+  }
+
+  template <class T, class U>
+  requires requires (T&& x, U&& y) { write_into(static_cast<T&&>(x), static_cast<U&&>(y)); }
+  constexpr void invoke (T&& target, U&& value, preference<0>)
+  noexcept(noexcept(write_into(static_cast<T&&>(target), static_cast<U&&>(value)))) {
+    write_into(static_cast<T&&>(target), static_cast<U&&>(value));
+  }
+
+};
 
 } /* namespace apex::detail::iter::write_into */
 
