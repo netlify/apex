b255e7132adcd8b0d0c91e8435749ca908f02006
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-08-04 10:34:31 -0700

    ✨ Add initial implementation for apex::iter::read_from

--- include/apex/iter/read-from.hpp
+++ include/apex/iter/read-from.hpp
@@ -3,7 +3,29 @@
 
 namespace apex::detail::iter::read_from {
 
-struct function final { };
+template <class T> void read_from (T const&) = delete;
+
+struct function final {
+  template <class T>
+  constexpr decltype(auto) operator () (T&& value) const
+  noexcept(noexcept(function::invoke(static_cast<T&&>(value), prefer<1>))) {
+    return function::invoke(static_cast<T&&>(value), prefer<1>);
+  }
+private:
+  template <class T>
+  requires requires (T&& x) { { static_cast<T&&>(x).read_from() } -> different_from<void>; }
+  static constexpr decltype(auto) invoke (T&& value, preference<1>)
+  noexcept(noexcept(static_cast<T&&>(value).read_from())) {
+    return static_cast<T&&>(value).read_from();
+  }
+
+  template <class T>
+  requires requires (T&& x) { { read_from(static_cast<T&&>(x)) } -> different_from<void>; }
+  static constexpr decltype(auto) invoke (T&& value, preference<0>)
+  noexcept(noexcept(read_from(static_cast<T&&>(value)))) {
+    return read_from(static_cast<T&&>(value));
+  }
+};
 
 } /* namespace apex::detail::iter::read_from */
 
