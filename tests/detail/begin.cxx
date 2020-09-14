40fd2f434ea8d7112631f86bdf9130152951fb0e
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-09-14 07:11:17 -0700

    ✅ Add tests for ranges::{begin,end} ADL lookup

--- tests/detail/begin.cxx
+++ tests/detail/begin.cxx
@@ -3,6 +3,12 @@
 #include <vector>
 #include <string>
 
+struct wrapped {
+  std::vector<int> x { 1, 2, 3, 4 };
+};
+
+decltype(auto) begin (wrapped& w) { return w.x.begin(); }
+
 TEST_CASE("ranges::begin member function") {
   std::vector<int> x { 1, 2, 3, 4 };
   CHECK(apex::ranges::begin(x) == x.begin());
@@ -11,4 +17,9 @@ TEST_CASE("ranges::begin member function") {
 TEST_CASE("ranges::begin array access") {
   int x[4] = { 1, 2, 3, 4 };
   CHECK(*apex::ranges::begin(x) == x[0]);
+}
+
+TEST_CASE("ranges::begin adl lookup") {
+  wrapped w;
+  CHECK(apex::ranges::begin(w) == w.x.begin());
 }
--- tests/detail/end.cxx
+++ tests/detail/end.cxx
@@ -1,6 +1,12 @@
 #include <apex/detail/prelude/end.hpp>
 #include <vector>
 
+struct wrapped {
+  std::vector<int> x { 1, 2, 3, 4 };
+};
+
+decltype(auto) end (wrapped& w) { return w.x.end(); }
+
 TEST_CASE("ranges::end member function") {
   std::vector<int> x { 1, 2, 3, 4 };
   CHECK(apex::ranges::end(x) == x.end());
@@ -9,4 +15,9 @@ TEST_CASE("ranges::end member function") {
 TEST_CASE("ranges::end array access") {
   int x[4] = { 1, 2, 3, 4 };
   CHECK(apex::ranges::end(x) == (x + 4));
+}
+
+TEST_CASE("ranges::end adl lookup") {
+  wrapped w;
+  CHECK(apex::ranges::end(w) == w.x.end());
 }
