f8c687982d703708559a1763c24f32ae1bed3d37
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-10-08 11:40:09 -0700

    🐛 Fix apex::span when <span> is available

--- src/core/span.cxx
+++ src/core/span.cxx
@@ -1,9 +1,13 @@
 #include <apex/core/span.hpp>
 #include <vector>
 
+#if not APEX_CHECK_API(span, 202002)
+
 namespace apex {
 
 template struct span<byte const, dynamic_extent>;
 template struct span<byte, dynamic_extent>;
 
 } /* namespace apex::v1 */
+
+#endif /* APEX_CHECK_API(span, 202002) */
